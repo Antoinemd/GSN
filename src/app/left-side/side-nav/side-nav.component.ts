a5004b3c68ce5940539094fac6d8a487cf29b578
Antoinemd <antoine.mnd@gmail.com>
2017-10-23 16:45:35 +0200

    added icons folder and game icons, json folder and json files for user and catalogueJeux. Added sidenav menu growing.

--- src/app/left-side/side-nav/side-nav.component.ts
+++ src/app/left-side/side-nav/side-nav.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavComponent implements OnInit {
 
+  // taille par défaut de la barre minimisée
+  sideNavWidth = 10;
+  sideOpacity = 1;
+
   constructor() { }
 
   ngOnInit() {
+    this.sideOpacity = 0.5;
+  }
+
+  widthUp(){
+    this.sideNavWidth = 200;  
+    this.sideOpacity = 1;
+    console.log("increase sidenav width");
+  }
+
+  widthDown(){
+    this.sideNavWidth = 10;
+    this.sideOpacity = 0.5;
+    console.log("decrease sidenav width");
   }
 
 }
